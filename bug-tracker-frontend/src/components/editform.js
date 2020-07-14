a7e948231bea51db2ee631ee9c6f43810717c52f
MoAziz123 <46410153+MoAziz123@users.noreply.github.com>
2020-07-14 14:49:34 +0100

    Update editform.js

--- bug-tracker-frontend/src/components/editform.js
+++ bug-tracker-frontend/src/components/editform.js
@@ -105,7 +105,7 @@ export class EditForm extends React.Component
                     }
                 })}
         else{
-            this.setState({message:{title:"Please ensure your title is less than 100 characters", description:this.state.message.description, log_date:this.state.message.log_date}})
+            this.setState({message:{title:"Please ensure your title is less than 10 characters", description:this.state.message.description, log_date:this.state.message.log_date}})
         }
     }
 
@@ -127,7 +127,7 @@ export class EditForm extends React.Component
                     }
                 })}
         else{
-            this.setState({message:{title:this.state.message.title, description:"Please ensure your description is less than 500 characters", log_date:this.state.message.log_date}})
+            this.setState({message:{title:this.state.message.title, description:"Please ensure your description is less than 50 characters", log_date:this.state.message.log_date}})
         }
     }
    
